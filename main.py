0c8afd6ca33bd72cc9f427743c3449293c00bdcf
thmmahendra <thmmahendra@gmail.com>
2024-11-26 08:45:24 +0545

    Initial Commits

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_date, get_description
 
 class CSV:
     CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date", "amount", "category","description"]
 
     @classmethod
     def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+            print("Entry added Successfully")
+
+
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of the transation (dd-mm-yy) or hit enter for today's date: ", allow_default=True)
+    amount = get_amount()
+    category= get_category()
+    description = get_description()
+    CSV.add_entry(date,amount,category,description)
+    
+add()
